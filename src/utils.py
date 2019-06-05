379873fe17ff258aa488274f5ff9f2e60a8b8eba
bakowroc <bakowroc@gmail.com>
2019-06-05 12:24:40 +0200

    Add new approach

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,6 @@ import string
 
 
 def generate_strings(length=4):
-    chars = string.ascii_letters + string.digits
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
     for item in itertools.product(chars, repeat=length):
         yield "".join(item)
