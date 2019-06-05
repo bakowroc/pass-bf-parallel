3f00d3d16c2c10424429848942a5dfbdd8a502d0
bakowroc <bakowroc@gmail.com>
2019-06-05 12:24:40 +0200

    Add new approach

--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@ import sys
 
 from master import Master
 
+NODES = ['10.11.220.20', '10.11.220.21', '10.11.220.22', '10.11.220.23']
+PASSWORD = sys.argv[1]
+USE_NODES = int(sys.argv[2])
+
 if __name__ == '__main__':
-    master = Master(sys.argv[1])
+    print(f'Running for {len(NODES[:USE_NODES])} nodes: {PASSWORD}')
+    master = Master(PASSWORD, nodes=NODES[:USE_NODES], partioning=1)
     master.start()
--- src/master.py
+++ src/master.py
@@ -1,18 +1,26 @@
 import dispy
+import sys
 
 from compute import compute
 from utils import generate_strings
 
 
 class Master:
-    def __init__(self, password, partioning=0):
+    def __init__(self, password, nodes, partioning=1):
+        self._nodes = nodes
         self._password = password
-        self._cluster = dispy.JobCluster(nodes=['10.11.220.20', '10.11.220.21', '10.11.220.22', '10.11.220.23'],
+        self._cluster = dispy.JobCluster(nodes=self._nodes,
                                          computation=compute,
                                          callback=self.job_callback)
         self._jobs = []
+        self._solution_report = {}
         self._finished = False
-        self._partioning = 0 if partioning == 0 else len(self._password) - partioning
+        self._partioning = len(self._password) - partioning
+        self._finished_time = 0
+
+        if partioning == 0:
+            raise Exception('Partioning has to be larger than 0')
+
         if partioning >= len(self._password):
             raise Exception('Partioning has to be smaller than password length')
 
@@ -20,6 +28,7 @@ class Master:
         job_id = 1
         for base_string in generate_strings(length=len(self._password) - self._partioning):
             if self._finished:
+                self._cluster.close(terminate=True)
                 break
 
             job = self._cluster.submit([self._password, base_string])
@@ -28,11 +37,29 @@ class Master:
             job_id += 1
 
         self._cluster.wait()
+        self.save_status()
+
+    def save_status(self):
+        file_name = f'{self._password}_{self._partioning}_{len(self._nodes)}'
+        orig_stdout = sys.stdout
+        f = open("./TESTS/" + file_name, 'w+')
+        f.write(f"SLAVE: {self._solution_report['slave']}\n")
+        f.write(f"JOB:  {self._solution_report['job']}\n")
+        f.write(f"SOLUTION: {self._solution_report['solution']}\n")
+        f.write("SUMMARY: \n")
+        sys.stdout = f
         self._cluster.print_status()
+
+        sys.stdout = orig_stdout
+        f.close()
+        print('FINISHED')
 
     def job_callback(self, job):
         if job.status == dispy.DispyJob.ProvisionalResult:
             if job.result[2]:
-                print(f'{job.result[0]} in JOB {job.id} found solution! It\'s {job.result[1]}')
+                self._solution_report = {
+                    'slave': job.result[0],
+                    'job': job.id,
+                    'solution': job.result[1]
+                }
                 self._finished = True
-                self._cluster.close(terminate=True)
